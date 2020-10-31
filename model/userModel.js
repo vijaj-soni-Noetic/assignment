85bcc63c54328b99cd6f772157d592e115ddd6f3
vijaj-soni-Noetic <30283637+vijaj-soni-Noetic@users.noreply.github.com>
2020-10-31 07:29:54 +0530

    Update userModel.js

--- model/userModel.js
+++ model/userModel.js
@@ -24,7 +24,18 @@ const userSchema = mongoose.Schema({
         select: false
     }
 });
+userSchema.pre('save', async function(next) {
+    // Only run this function if modified
+    if(!this.isModified('password')) return next();
 
+    this.password = await bcrypt.hash(this.password, 12);
+
+    this.passwordConfirm = undefined
+    next();
+});
+userSchema.methods.correctPassword = async function(candidatePassword, userPassword){
+    return await bcrypt.compare(candidatePassword, userPassword);
+}
 
 const User = mongoose.model('User', userSchema);
 
